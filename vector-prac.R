67c9c4c10c35e4f52ea588c394a51f379419b4e3
ishamuk <ishamuk@bu.edu>
2025-01-27 16:14:19 -0500

    fixed naming system of work for question 2

---
+++ vector-prac.R
@@ -0,0 +1,13 @@
+# 1
+c(1:20) # a
+c(20:1) # b
+c(1:20,19:1) #c
+tmp <- c(4,6,3) #d
+rep(tmp,10) #e
+rep(tmp,11)[1:31] #f
+rep(tmp, c(10,20,30)) #g
+
+# 2
+x <- seq(from = 3, to = 6, by = 0.1) #h
+exp(x)*cos(x)
+
